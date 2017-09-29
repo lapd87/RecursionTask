acac79cb55a0e5e483d2c9fda27ff08f4d384190
lapd87 <mario87lapd@gmail.com>
2017-09-29 15:41:23 +0300

    adding solved recursion task

---
+++ recursion/recursion/recursion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class recursion
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string professor(int i)
+        {
+            if (i == 1)
+            {
+                return "1";
+            }
+            else
+            {
+                return professor(i - 1) + i + professor(i - 1);
+            }
+        }
+        Console.WriteLine(professor(n));
+    }
+}
